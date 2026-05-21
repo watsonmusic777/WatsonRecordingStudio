5528cd5680cf2dbd2f5f2894836151d00af538e5
watsonmusic777 <watsonmusic777@gmail.com>
2026-05-20 14:26:20 -1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+// JavaScript Document
+// Later you’ll replace static content with API calls to your backend.
+// Example:
+// fetch('https://api.watsonrecoedingatudio.com/api/tracks')
+//   .then(res => res.json())
+//   .then(data => renderTracks(data));
